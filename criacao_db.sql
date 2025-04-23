fbda768b424ed710535b65ff4bd8b60ef548a596
Eduardo Aguiar Leite da Silva <dudu170606@gmail.com>
2025-04-23 10:20:06 -0300

    Criação da estrutura do banco de dados
    Criei as tabelas de aluno, professor, pergunta e resposta

---
+++ criacao_db.sql
@@ -0,0 +1,46 @@
+drop database if exists show_poliedro;
+create database if not exists show_poliedro;
+use show_poliedro;
+
+create table Aluno (
+id_aluno int primary key auto_increment,
+nome_aluno varchar(50) not null,
+email varchar(50) not null unique,
+senha varchar(50) not null,
+pontuacao int,
+respostas_corretas int,
+respostas_erradas int);
+
+describe Aluno;
+
+create table Professor (
+id_professor int primary key auto_increment,
+nome_professor varchar(50) not null,
+email varchar(50) not null unique,
+senha varchar(50) not null,
+pontuacao int,
+respostas_corretas int,
+respostas_erradas int);
+
+describe Professor;
+
+create table Pergunta (
+id_pergunta int primary key auto_increment,
+materia varchar(20) not null,
+pergunta varchar(70) not null unique,
+dificuldade varchar(7) not null,
+id_professor int,
+foreign key(id_professor) references Professor(id_professor));
+
+describe Pergunta;
+
+create table Resposta(
+id_reposta int primary key auto_increment,
+resposta_correta varchar(50) not null,
+resposta_um varchar(50) not null,
+resposta_dois varchar(50) not null,
+resposta_tres varchar(50) not null,
+id_pergunta int not null,
+foreign key(id_pergunta) references Pergunta(id_pergunta));
+
+describe Resposta;
